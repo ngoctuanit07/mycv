f65ff440e9ce8fe48b282e3bf01fc2726489a0fb
Tuan Nguyen <ngoctuanit07@gmail.com>
2024-01-31 21:17:43 +0700

    Updates

---
+++ precache-manifest.5ab6dff9ee43d0e36ffa01b826256e9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eb7c9cbda5583cc9c34b001fb34a958",
+    "url": "/index.html"
+  },
+  {
+    "revision": "802363bef48f89393bb9",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "6deae8e8c719718add7f",
+    "url": "/static/js/2.7616a499.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/static/js/2.7616a499.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "802363bef48f89393bb9",
+    "url": "/static/js/main.bfd2699b.chunk.js"
+  },
+  {
+    "revision": "f5e337750815ed0ba0e7",
+    "url": "/static/js/runtime-main.a0044592.js"
+  }
+]);
